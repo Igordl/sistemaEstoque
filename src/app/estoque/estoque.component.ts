da901ef37e042c71e265ea1b7c031a3c6cacf300
igordl <igor.lucena@ccc.ufcg.edu.br>
2019-10-01 16:41:22 -0300

    ajustando versão

--- src/app/estoque/estoque.component.ts
+++ src/app/estoque/estoque.component.ts
@@ -2,8 +2,10 @@ import { RouterModule, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { getMatAutocompleteMissingPanelError } from '@angular/material/autocomplete';
 import { LoginComponent } from '../login/login.component';
-import { AuthService } from 'angularx-social-login';
+import { AuthService, SocialUser } from 'angularx-social-login';
 import { HttpClient } from 'selenium-webdriver/http';
+import { NgStyle } from '@angular/common';
+import { isNumber } from 'util';
 
 
 @Component({
@@ -12,27 +14,23 @@ import { HttpClient } from 'selenium-webdriver/http';
   styleUrls: ['./estoque.component.css']
 })
 export class EstoqueComponent implements OnInit {
+  private user: SocialUser;
+  loggedIn: boolean;
   categoria: string;
 
   item: {nomeCategoria: string, nomeItem: string, valorItem: string,
-    qntEstoque: string, imagemItem: string};
+    qntItem: number, imagemItem: string, click: boolean};
 
-  itemAtual = {
-    nomeCategoria: '',
-    nomeItem: '',
-    valorItem: '',
-    qntItem: '',
-    imagemItem: '',
-    click: false
-  };
+    itemAtual = {nomeCategoria: '', nomeItem: '', valorItem: '',
+    qntItem: 0, imagemItem: '', click: false};
 
   categorias: ['Celular', 'Eletrodoméstico', 'Tv', 'Videogame'];
 
   itensEstoque = [
-    {nomeCategoria: 'Celular', nomeItem: 'mi a1', valorItem: '200', qntEstoque: '137', imagemItem: '', click: false},
-    {nomeCategoria: 'Eletrodoméstico', nomeItem: 'v3n4i3', valorItem: '34g34g', qntEstoque: '456', imagemItem: '', click: false},
-    {nomeCategoria: 'Tv', nomeItem: 'r34r', valorItem: '4g34g', qntEstoque: '346', imagemItem: '', click: false},
-    {nomeCategoria: 'Videogame', nomeItem: '4g3g', valorItem: '34g34g', qntEstoque: '36', imagemItem: '', click: false}
+    {nomeCategoria: 'Celular', nomeItem: 'iPhone 6', valorItem: '1000', qntItem: 137, imagemItem: '', click: false},
+    {nomeCategoria: 'Eletrodoméstico', nomeItem: 'v3n4i3', valorItem: '34g34g', qntItem: 456, imagemItem: '', click: false},
+    {nomeCategoria: 'Tv', nomeItem: 'r34r', valorItem: '4g34g', qntItem: 36, imagemItem: '', click: false},
+    {nomeCategoria: 'Videogame', nomeItem: '4g3g', valorItem: '34g34g', qntItem: 34, imagemItem: '', click: false}
   ];
 
   router: Router;
@@ -40,10 +38,13 @@ export class EstoqueComponent implements OnInit {
   constructor(private authService: AuthService, rota: Router) {
     this.router = rota;
     this.ngOnInit;
+    this.authService.authState.subscribe((user) => {
 
+      this.user = user;
 
-    
+      this.loggedIn = (user != null);
 
+    });
    }
 
 
@@ -56,7 +57,10 @@ export class EstoqueComponent implements OnInit {
   trocaCategoria(categoriaItem: string) {
     this.categoria = categoriaItem;
     console.log(this.categoria);
+    for (let i = 0; i < this.itensEstoque.length; i++) {
+      this.itensEstoque[i].click = false;
 
+    }
   }
 
   addItem(item) {
@@ -80,10 +84,10 @@ export class EstoqueComponent implements OnInit {
 
   sortEstoque() {
     this.itensEstoque.sort((a, b) => {
-      if (a.qntEstoque > b.qntEstoque) {
+      if (a.qntItem > b.qntItem) {
         return -1;
       }
-      if (a.qntEstoque < b.qntEstoque) {
+      if (a.qntItem < b.qntItem) {
         return 1;
       }
       return 0;
@@ -107,7 +111,7 @@ export class EstoqueComponent implements OnInit {
       nomeCategoria: '',
       nomeItem: '',
       valorItem: '',
-      qntItem: '',
+      qntItem: 0,
       imagemItem: '',
       click: false
     };
@@ -117,6 +121,22 @@ export class EstoqueComponent implements OnInit {
   signOut(){
       this.authService.signOut();
       this.router.navigate(['']);
+  }
+
+  getColor(qntItem){
+    if ( qntItem > 10) {
+      return 'rgba(154, 255, 145, 0.350)';
+      console.log(qntItem + " verde");
+    }
+    if ( qntItem == 0) {
+      return 'rgba(255, 145, 145, 0.350)';
+      console.log(qntItem + " vermelho");
+    }
+    if( qntItem > 0 && qntItem <= 10){
+      return 'rgba(250, 235, 99, 0.350)';
+      console.log(qntItem + " amarelo");
+    }
+
   }
 
 
